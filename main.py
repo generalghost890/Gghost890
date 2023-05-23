be1e4158eee732c84193475e702962e83b9c68be
generalghost890 <116657729+generalghost890@users.noreply.github.com>
2023-05-23 09:42:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/generalghost890/PrivaPact/")
+os.system("git clone https://github.com/generalghost890/Gghost890/")
 os.system("cd hacks && python bot.py")
