be34475e949871aa607f550839a6f1c1e85375ad
generalghost890 <116657729+generalghost890@users.noreply.github.com>
2023-06-23 13:03:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,8 @@ from telethon.sync import TelegramClient, events
 from telethon.sessions import MemorySession
 from os import system
 from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantAdmin, ChannelParticipantCreator
-api_hash = os.environ["API_HASH"]
-bot_token = os.environ["BOT_TOKEN"]
+api_hash = ["API_HASH"]
+bot_token = ["BOT_TOKEN"]
 client = TelegramClient('ofgoqwieufg', api_id, api_hash).start(bot_token=bot_token)
 from telethon import TelegramClient as tg
 from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest as pc, JoinChannelRequest as join, LeaveChannelRequest as leave, DeleteChannelRequest as dc
